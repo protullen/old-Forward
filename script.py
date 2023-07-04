3f01ef00cfe5514ee3e3f12d7dfc6db95388ab78
Xprotullen <138602811+xprotullen@users.noreply.github.com>
2023-07-05 00:11:12 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+class ChatMSG(object):
+    HELP_TXT = "HI"
+
+    FORWARDING = f"""<i>File Forwarding Started😉</i>
+<b>From Chat:</b> {from_chat.name}
+<b>ID:</b> <code>{from_chat.id}</code>
+
+<b>To Chat:</b> {to_chat.name}
+<b>ID:</b> <code>{to_chat.id}</code>
+
+start message ID: <code>{start_id}</code>
+end message ID: <code>{stop_id}</code>
+"""
