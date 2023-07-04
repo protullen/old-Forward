b407309317c2d5b835891ff3e6f51d1a611811f2
Xprotullen <138602811+xprotullen@users.noreply.github.com>
2023-07-04 23:41:28 +0530

    Update old_forward.py

--- plugins/old_forward.py
+++ plugins/old_forward.py
@@ -1,20 +1,18 @@
 # Lx 0980
-# Year : 2023
+# Year: 2023
 
-import asyncio, sys, os
+import asyncio
+import sys
+import os
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait
-from info import AUTH_USERS 
+from info import AUTH_USERS
 
 import logging
-
 logger = logging.getLogger(__name__)
 
-DOCUMENT = enums.MessagesFilter.DOCUMENT 
-VIDEOS = enums.MessagesFilter.VIDEO
 user_file_types = {}
-
 is_forwarding = False
 
 @Client.on_message(filters.private & filters.command(["clone"]))
@@ -35,7 +33,7 @@ async def run(bot, message):
     delay_time = int(message_text[5])
 
     m = await bot.send_message(
-        text="<i>File Forwarding Started😉</i>",        
+        text="<i>File Forwarding Started😉</i>",
         chat_id=message.chat.id
     )
 
@@ -47,13 +45,10 @@ async def run(bot, message):
         file_types = enums.MessagesFilter.DOCUMENT
     elif "videos" in forward_type:
         file_types = enums.MessagesFilter.VIDEO
-        
     else:
         file_types = enums.MessagesFilter.VIDEO
-        
-        
-    
-    async for message in bot.USER.search_messages(chat_id=FROM, filter=VIDEOS):
+
+    async for message in bot.USER.search_messages(chat_id=FROM, filter=file_types):
         try:
             if not is_forwarding:
                 break
@@ -64,7 +59,7 @@ async def run(bot, message):
             elif message.document:
                 file_name = message.document.file_name
             elif message.audio:
-                file_name = message.audio.file_name 
+                file_name = message.audio.file_name
             else:
                 file_name = None
             try:
@@ -75,7 +70,7 @@ async def run(bot, message):
                     caption=f"**{message.caption}**",
                     message_id=message.id
                 )
-            else:
+            except Exception:
                 await bot.USER.copy_message(
                     chat_id=TO,
                     from_chat_id=FROM,
@@ -86,48 +81,46 @@ async def run(bot, message):
             files_count += 1
             await asyncio.sleep(delay_time)
         except FloodWait as e:
-            await asyncio.sleep(e.value) 
+            await asyncio.sleep(e.value)
         except Exception as e:
             print(e)
             pass
 
     is_forwarding = False
-    
+
     await m.edit(
-        text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files:-</b> <code>{files_count}</code> <b>Files</b>\n<b>Thanks For Using Me❤️</b>",        
+        text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files: {files_count}</b> Files\n<b>Thanks For Using Me ❤️</b>",
     )
 
- 
+
 @Client.on_message(filters.private & filters.command(["stop"]))
 async def stop_forwarding(bot, message):
     global is_forwarding
     if message.from_user.id not in AUTH_USERS:
         return
-    
+
     if is_forwarding:
         is_forwarding = False
         await message.reply_text("File forwarding process stopped.")
     else:
         await message.reply_text("File forwarding process is not running.")
 
+
 @Client.on_message(filters.private & filters.command(["set_file_type"]))
 async def set_file_type(bot, message):
     if message.from_user.id not in AUTH_USERS:
-        return await message.reply_text("You are not authorised uset")
+        return await message.reply_text("You are not authorized to use this command.")
     user_id = str(message.from_user.id)
     text = message.text.lower()
-    if "files" in text or "file" in text :
-        file_type = "document "
+    if "files" in text or "file" in text:
+        file_type = "document"
     elif "video" in text or "videos" in text:
         file_type = "videos"
     else:
         await message.reply_text("Invalid file type. Please specify either 'files' or 'videos'.")
-        return 
+        return
     user_file_types[user_id] = file_type
     forward_type = user_file_types.get(user_id)
     if not forward_type:
-        return await message.reply_text("Error setting document forwarding type\n❌ ❌ ❌")     
+        return await message.reply_text("Error setting document forwarding type\n❌ ❌ ❌")
     await message.reply_text(f"Forward type set to: {forward_type.capitalize()} ✅ ✅ ✅")
-
-
-
