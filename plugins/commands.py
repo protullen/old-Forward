c8bfe760a15773ceb2a683c4d413be408abb9aca
Lx 0980 <133576014+TheLx0980@users.noreply.github.com>
2023-07-05 08:59:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,23 +1,24 @@
-# (c)  Lx 0980
+# (c) Lx 0980
 
 import logging
 logger = logging.getLogger(__name__)
 
-from info import AUTH_USERS 
+from info import AUTH_USERS
 from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
-    if message.from.user.id not in AUTH_USERS:
+    if message.from_user.id not in AUTH_USERS:
         return await message.reply("This bot is not for public use")
     await message.reply(
-        text=f"""Hello {message.from.user.mention}
+        text=f"""Hello {message.from_user.mention}
 <i>I'm Simple Auto file Forward User Bot
-This Bot forward all old videos from One channel to Your Personal channel
+This Bot forwards all old videos from One channel to Your Personal channel
 More details <code>/userbot_help</code></i>
         """,
         disable_web_page_preview=True,
         quote=True
+    )
 
 @Client.on_message(filters.command("userbot_help") & filters.private & filters.incoming)
 async def help(client, message):
@@ -28,12 +29,12 @@ async def help(client, message):
 <b>• Then send any message In your personal telegram channel</b>
 <b>• Then use /clone command in your bot</b>
 
-<b><u>Available Command</b></u>
+<b><u>Available Commands</b></u>
 
 * /userbot_start - <b>Bot Alive</b>
 * /userbot_help - <b>more help</b>
 * /clone - <b>start forward</b>
-       format = <code>/clone from_id to_id start_id end_id delay_second</code> <b> separate with space</b> example : <code> /clone -10077775444 -10073774747 34 3747 23</code>
+       format = <code>/clone from_id to_id start_id end_id delay_second</code> <b> separate with space</b> example: <code>/clone -10077775444 -10073774747 34 3747 23</code>
 
 * /userbot_about - <b>About Me</b>
 
@@ -42,4 +43,6 @@ async def help(client, message):
         disable_web_page_preview=True,
         quote=True
     )
-    
+
+
+
