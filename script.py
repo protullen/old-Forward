2920507d5f10e2561e282a21db8bde4f87603d23
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-08 14:02:48 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,20 @@ class ChatMSG(object):
     HELP_TXT = "HI"
 
     FORWARDING = f"""Forwarding Started! ✅
-        
+    
 <b>• Source Chat:</b> {}
 <b>• Target Chat:</b> {}
 <b>• Start Msg ID:</b> <a href='{}'>{}</a>
 <b>• End Msg ID:</b> <a href='{}'>{}</a>
+<b>• Status:</b> Forwarding 
+"""
+    FORWARDING_STOPPED =f"""Forwarding Stopped!
+    
+<b>• Source Chat:</b> {}
+<b>• Target Chat:</b> {}
+<b>• Start Msg ID:</b> <a href='{}'>{}</a>
+<b>• End Msg ID:</b> <a href='{}'>{}</a>
+<b>• Status:</b> Complete ✅
 
-/cancel - Cancel Forwarding
+Successfully Forwarded {} {}
 """
