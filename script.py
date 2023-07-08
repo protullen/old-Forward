9bf2bc18d69b97737eaa524d5ed7a17a91b60d5c
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-08 14:32:07 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,14 @@
 class ChatMSG(object):
+    HELP_TXT = "HI"
 
     @staticmethod
     def FORWARDING():
         return """Forwarding Started! ✅
 
-<b>• Source Chat:</b> {}
-<b>• Target Chat:</b> {}
-<b>• Start Msg ID:</b> <a href='{}'>{}</a>
-<b>• End Msg ID:</b> <a href='{}'>{}</a>
+<b>• Source Chat:</b> {{}}
+<b>• Target Chat:</b> {{}}
+<b>• Start Msg ID:</b> <a href='{{}}'>{{}}</a>
+<b>• End Msg ID:</b> <a href='{{}}'>{{}}</a>
 <b>• Status:</b> Forwarding 
 """
 
@@ -15,10 +16,10 @@ class ChatMSG(object):
     def FORWARDING_STOPPED(files_count, forward_type):
         return f"""Forwarding Stopped!
 
-<b>• Source Chat:</b> {}
-<b>• Target Chat:</b> {}
-<b>• Start Msg ID:</b> <a href='{}'>{}</a>
-<b>• End Msg ID:</b> <a href='{}'>{}</a>
+<b>• Source Chat:</b> {{}}
+<b>• Target Chat:</b> {{}}
+<b>• Start Msg ID:</b> <a href='{{}}'>{{}}</a>
+<b>• End Msg ID:</b> <a href='{{}}'>{{}}</a>
 <b>• Status:</b> Complete ✅
 
 Successfully Forwarded {files_count} {forward_type}
