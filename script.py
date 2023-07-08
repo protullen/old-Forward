e35971881be4ad2af0b6bd18df4245393ebd355e
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-08 15:24:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -12,8 +12,8 @@ class ChatMSG(object):
 
 <b>• Source Chat:</b> {}
 <b>• Target Chat:</b> {}
-<b>• Start Msg ID:</b> <a href='{}'>{{}}</a>
-<b>• End Msg ID:</b> <a href='{}'>{{}}</a>
+<b>• Start Msg ID:</b> <a href='{}'>{}</a>
+<b>• End Msg ID:</b> <a href='{}'>{}</a>
 <b>• Status:</b> Complete ✅
 
 Successfully Forwarded {} {}
