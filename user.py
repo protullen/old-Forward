b889e01404a9a687287613898465716462bccb34
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-08 09:58:20 +0530

    Update user.py

--- user.py
+++ user.py
@@ -17,9 +17,9 @@ class User(Client):
 
     async def start(self):
         await super().start()
-        usr_bot_me = await self.get_me()
+        me = await self.get_me()
         self.LOGGER(__name__).info(
-            f"{usr_bot_me.username} Userbot started! 👤 "
+            f"{me.username} Userbot started! 👤 "
         )
         return (self, usr_bot_me.id)
 
